40c935dd7ad8db9a536862ab6540588fedd19b1d
Sebastian Toledo <seabastoledo009@gmail.com>
2024-06-10 01:04:39 -0500

    Creacion del controlador del cliente

--- src/associations/associations.js
+++ src/associations/associations.js
@@ -6,11 +6,14 @@ import { FacturaCabecera } from '../models/facturaCabecera.js';
 import { FacturaDetalle } from '../models/facturaDetalle.js';
 
 // Definir las relaciones
+Usuario.hasMany(Cliente, { foreignKey: 'usuario_codigo' });
+Cliente.belongsTo(Usuario, { foreignKey: 'usuario_codigo' });
+
+Habitacion.hasMany(Reserva, { foreignKey: 'habitacion_codigo' });
+Reserva.belongsTo(Habitacion, { foreignKey: 'habitacion_codigo' });
+
 Cliente.hasMany(Reserva, { foreignKey: 'cliente_codigo' });
 Reserva.belongsTo(Cliente, { foreignKey: 'cliente_codigo' });
-
-Cliente.hasMany(FacturaCabecera, { foreignKey: 'cliente_codigo' });
-FacturaCabecera.belongsTo(Cliente, { foreignKey: 'cliente_codigo' });
 
 Reserva.hasMany(FacturaDetalle, { foreignKey: 'reserva_codigo' });
 FacturaDetalle.belongsTo(Reserva, { foreignKey: 'reserva_codigo' });
@@ -18,17 +21,14 @@ FacturaDetalle.belongsTo(Reserva, { foreignKey: 'reserva_codigo' });
 FacturaCabecera.hasMany(FacturaDetalle, { foreignKey: 'factura_numero' });
 FacturaDetalle.belongsTo(FacturaCabecera, { foreignKey: 'factura_numero' });
 
-Habitacion.hasMany(Reserva, { foreignKey: 'habitacion_codigo' });
-Reserva.belongsTo(Habitacion, { foreignKey: 'habitacion_codigo' });
-
-Usuario.hasMany(Cliente, { foreignKey: 'usuario_codigo' });
-Cliente.belongsTo(Usuario, { foreignKey: 'usuario_codigo' });
+Cliente.hasMany(FacturaCabecera, { foreignKey: 'cliente_codigo' });
+FacturaCabecera.belongsTo(Cliente, { foreignKey: 'cliente_codigo' });
 
 export {
     Usuario,
+    Cliente,
     Habitacion,
     Reserva,
-    FacturaCabecera,
     FacturaDetalle,
-    Cliente
+    FacturaCabecera
 };
--- src/controllers/cliente.controlador.js
+++ src/controllers/cliente.controlador.js
@@ -1,44 +1,120 @@
-import { pool } from '../database/database.js';
+import { sequelize } from '../database/database.js';
+import { Cliente } from '../models/cliente.js';
 
 export class clienteControlador {};
 
 export let listar = async (req, res) => {
-    const { rows } = await pool.query('SELECT * FROM cliente');
-    res.json(rows);
+    try {
+        const result = await sequelize.transaction( async t => {
+            const clientes = await Cliente.findAll( { transaction: t });
+            return clientes;
+        });
+        res.json(result);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export let crear = async (req, res) => {
     const data = req.body;
-    console.log(data);
-    const { rows } = await pool.query('INSERT INTO cliente ($1) ' +
-        'values() RETURNING *', [data.name]);
-    return res.json(rows[0]);
+    console.log('Datos cliente: ', data);
+    try {
+        const result = await sequelize.transaction(async t => {
+            const cliente = await Cliente.create(data, { transaction: t });
+            return cliente;
+        });
+        console.log(result);
+        return res.json(result);
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json(
+            { message: "Error en el ingreso de datos del cliente" }
+        );
+    }
 };
 
 export let listarInfo = async (req, res) => {
     const { idCliente } = req.params;
-    const { rows } = await pool.query('SELECT * FROM cliente WHERE id = $1',
-        [idCliente]);
-    if (rows.length == 0){
-        return res.status(404).json({ message: 'Cliente no encontrado' });
+    console.log('Buscando cliente con código: ', idCliente);
+    try {
+        const result = await sequelize.transaction( async t => {
+            const clientes = await Cliente.findAll({
+                where: {
+                    codigo_cliente: idCliente
+                },
+            },
+            {
+                    transaction: t
+            });
+            return clientes;
+        });
+        console.log(result);
+        if(result.length == 0){
+            return res.status(404).json({ message: "Cliente no encontrado" });
+        }
+        return res.json(result);
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({ message: "Cliente no encontrado" });
     }
-    res.json(rows[0]);
 };
 
 export let borrar = async (req, res) => {
     const { idCliente } = req.params;
-    const { rows, rowCount } = await pool.query('DELETE FROM cliente WHERE id = $1 RETURNING *', [idCliente]);
-    console.log(rows);
-    if(rowCount === 0) {
-        return res.status(404).json({ message: 'Cliente no encontrado'});
+    console.log('Borrando el cliente con código: ', idCliente);
+    try {
+        const result = await sequelize.transaction( async t => {
+            const clientes = await Cliente.destroy({
+                where: {
+                    codigo_cliente: idCliente
+                },
+            },
+            {
+                    transaction: t
+            });
+            return clientes;
+        });
+        console.log(result);
+        if(result.length == 0){
+            return res.status(404).json({ message: "Cliente no encontrado" });
+        }
+        return res.status(200).json(
+            { message: "Se eliminó el cliente con código " + idCliente }
+        );
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({ message: "Cliente no encontrado" });
     }
-    return res.sendStatus(204);
 };
 
-export let actualizar = async(req, res) => {
+export let actualizar = async (req, res) => {
     const { idCliente } = req.params;
     const data = req.body;
-    const { rows } = await pool.query('UPDATE cliente SET');
-    console.log(result);
-    return res.json(rows[0]);
+    console.log("Actualizando el cliente con código: ", idCliente);
+    try {
+        const result = await sequelize.transaction( async t => {
+            const clientes = await Cliente.update(
+                data,
+                {
+                    where: {
+                        codigo_cliente: idCliente
+                    },
+                },
+                {
+                    transaction: t
+                }
+            );
+            return clientes;
+        });
+        console.log(result);
+        if(result.length == 0){
+            return res.status(404).json({ message: "Cliente no encontrado" });
+        }
+        return res.status(200).json(
+            { message: "Se actualizó el cliente con código " + idCliente }
+        );
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({ message: "Cliente no encontrado" });
+    }
 };
