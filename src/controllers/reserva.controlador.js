b5c53dffae0e278882b47c3a6db17d3dd1a5c160
Sebastian Toledo <seabastoledo009@gmail.com>
2024-06-10 01:17:32 -0500

    Creacion del controlador de la reserva

--- src/controllers/reserva.controlador.js
+++ src/controllers/reserva.controlador.js
@@ -1,42 +1,121 @@
-import { pool } from '../database/database.js';
+import { sequelize } from '../database/database.js';
+import { Reserva } from '../models/reserva.js';
 
 export class reservaControlador {};
 
 export let listar = async (req, res) => {
-    const { rows } = await pool.query('SELECT * FROM reserva');
-    res.json(rows);
+    try {
+        const result = await sequelize.transaction( async t => {
+            const reservas = await Reserva.findAll( { transaction: t });
+            return reservas;
+        });
+        res.json(result);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export let crear = async (req, res) => {
     const data = req.body;
-    console.log(data);
-    const { rows } = await pool.query('INSERT INTO reserva ($1) values () RETURNING *', [data.name]);
-    return res.json(rows[0]);
+    console.log('Datos reserva: ', data);
+    try {
+        const result = await sequelize.transaction(async t => {
+            const reserva = await Reserva.create(data, { transaction: t });
+            return reserva;
+        });
+        console.log(result);
+        return res.json(result);
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json(
+            { message: "Error en el ingreso de datos de la reserva" }
+        );
+    }
 };
 
 export let listarInfo = async (req, res) => {
     const { idReserva } = req.params;
-    const { rows } = await pool.query('SELECT * FROM reserva WHERE id = $1',[idReserva]);
-    if (rows.length == 0){
-        return res.status(404).json({ message: 'Reserva no encontrada' });
+    console.log('Buscando reserva con código: ', idReserva);
+    try {
+        const result = await sequelize.transaction( async t => {
+            const reservas = await Reserva.findAll({
+                where: {
+                    codigo_reserva: idReserva
+                },
+            },
+            {
+                    transaction: t
+            });
+            return reservas;
+        });
+        console.log(result);
+        if(result.length == 0){
+            return res.status(404).json({ message: "Reserva no encontrada" });
+        }
+        return res.json(result);
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({ message: "Reserva no encontrada" });
     }
-    res.json(rows[0]);
 };
 
 export let borrar = async (req, res) => {
     const { idReserva } = req.params;
-    const { rows, rowCount } = await pool.query('DELETE FROM reserva WHERE  id = $1 RETURNING *', [idReserva]);
-    console.log(rows);
-    if(rowCount === 0) {
-        return res.status(404).json({ message: 'Reserva no encontrada' });
+    console.log('Borrando la reserva con código: ', idReserva);
+    try {
+        const result = await sequelize.transaction( async t => {
+            const reservas = await Reserva.destroy({
+                where: {
+                    codigo_reserva: idReserva
+                },
+            },
+            {
+                    transaction: t
+            });
+            return reservas;
+        });
+        console.log(result);
+        if(result.length == 0){
+            return res.status(404).json({ message: "Reserva no encontrada" });
+        }
+        return res.status(200).json(
+            { message: "Se eliminó la reserva con código " + idReserva }
+        );
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({ message: "Reserva no encontrada" });
     }
-    return res.sendStatus(204);
 };
 
-export let actualizar = async(req, res) => {
+export let actualizar = async (req, res) => {
     const { idReserva } = req.params;
     const data = req.body;
-    const { rows } = await pool.query('UPDATE reserva SET');
-    console.log(result);
-    return res.json(rows[0]);
+    console.log("Actualizando la reserva con código: ", idReserva);
+    try {
+        const result = await sequelize.transaction( async t => {
+            const reservas = await Reserva.update(
+                data,
+                {
+                    where: {
+                        codigo_reserva: idReserva
+                    },
+                },
+                {
+                    transaction: t
+                }
+            );
+            return reservas;
+        });
+        console.log(result);
+        if(result.length == 0){
+            return res.status(404).json({ message: "Reserva no encontrada" });
+        }
+        return res.status(200).json(
+            { message: "Se actualizó la reserva con código " + idReserva }
+        );
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json({ message: "Reserva no encontrada" });
+    }
 };
+
